d6f69c4af7630218a539bd9bbfef559374f0f3e3
Dominik Jetzen <djetzen@users.noreply.github.com>
2022-12-02 14:13:56 +0100

    Add solution for first day (#2)

---
+++ src/test/kotlin/de/djetzen/advent/one/CaloryCalculatorTest.kt
@@ -0,0 +1,53 @@
+package de.djetzen.advent.one
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CaloryCalculatorTest {
+
+    @Test
+    fun TestFileIsReadAndSplitAccordingly() {
+        val caloryCalculator = CaloryCalculator()
+
+        val caloryPerElve = caloryCalculator.readFileAndExtractCaloriesPerElve("src/test/resources/testInputs/testinput1_1.txt")
+
+        assertEquals(4,caloryPerElve.size)
+    }
+
+    @Test
+    fun testElveWithMaximumCaloriesIsFound() {
+        val caloryCalculator = CaloryCalculator()
+        val fewCaloriesCarried = intArrayOf(1,2,3)
+        val moreCaloriesCarried = intArrayOf(2,3,4)
+        val mostCaloriesCarried = intArrayOf(3,4,5)
+
+        val caloriesList = listOf(fewCaloriesCarried,moreCaloriesCarried,mostCaloriesCarried)
+
+        val mostCaloryCountFound = caloryCalculator.getMostCaloriesPerElve(caloriesList)
+
+        assertEquals(12,mostCaloryCountFound)
+    }
+
+    @Test
+    fun findElveCarryingMostCalories() {
+        val caloryCalculator = CaloryCalculator()
+        val extractedCaloriesPerElve =
+            caloryCalculator.readFileAndExtractCaloriesPerElve("src/test/resources/realInputs/realinput1_1.txt")
+
+        val mostCaloriesPerElve = caloryCalculator.getMostCaloriesPerElve(extractedCaloriesPerElve)
+
+        assertEquals(70764,mostCaloriesPerElve)
+    }
+
+    @Test
+    fun findSumOfTopThreeElvesCarryingCalories() {
+        val caloryCalculator = CaloryCalculator()
+
+        val extractedCaloriesPerElve =
+            caloryCalculator.readFileAndExtractCaloriesPerElve("src/test/resources/realInputs/realinput1_1.txt")
+
+        val mostCaloriesPerElve = caloryCalculator.getSumOfCaloriesForTopThreeElves(extractedCaloriesPerElve)
+
+        assertEquals(203905,mostCaloriesPerElve)
+    }
+}
