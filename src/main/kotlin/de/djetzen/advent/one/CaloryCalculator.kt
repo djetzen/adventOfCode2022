214776e7ca10ce694e739012de5ce8debd746e2e
DJetzen <djetzen@users.noreply.github.com>
2022-12-02 13:57:22 +0100

    Add solution for first day

---
+++ src/main/kotlin/de/djetzen/advent/one/CaloryCalculator.kt
@@ -0,0 +1,36 @@
+package de.djetzen.advent.one
+
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.regex.Pattern
+
+class CaloryCalculator {
+
+    fun readFileAndExtractCaloriesPerElve(fileName: String): List<IntArray> {
+        val readAllLines = Files.readAllLines(Paths.get(fileName))
+
+        val caloriesList = ArrayList<IntArray>()
+
+        var caloryPerElve = intArrayOf()
+        for (line in readAllLines) {
+            if(line.isBlank() || readAllLines.last()==line) {
+                caloriesList+=caloryPerElve
+                caloryPerElve = intArrayOf()
+            }
+            else {
+                caloryPerElve+=Integer.parseInt(line!!)
+            }
+        }
+        return caloriesList
+
+    }
+
+    fun getMostCaloriesPerElve(caloriesList: List<IntArray>): Int? {
+        return caloriesList.maxOfOrNull { it.sum() }
+    }
+
+    fun getSumOfCaloriesForTopThreeElves(caloriesList: List<IntArray>): Int {
+        return caloriesList.map{it.sum()}.sortedDescending().take(3).sum()
+    }
+
+}
